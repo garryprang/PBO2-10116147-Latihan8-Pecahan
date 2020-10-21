7e8668fe48027f8aee48c9e0be83278ec4e58e8e
grryv <grryv@grry>
2020-10-21 19:40:50 +0700

    Tugas - Latihan Pertemuan 1 Garry Prang 10116147 PBO-2

---
+++ src/pbo2/pkg10116147/latihan8/pecahan/PBO210116147Latihan8PPecahan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pbo2.pkg10116147.latihan8.pecahan;
+
+/**
+ * @author
+ * Nama                 : Garry Prang
+ * NIM                  : 10116147
+ * Kelas                : PBO-2
+ * Deskripsi Program    : Program untuk menampilkan tipe-tipe data dasar (bilangan).
+ */
+
+public class PBO210116147Latihan8PPecahan {
+    /**
+     * @param args the command line arguments
+     */
+        public static void main(String[] args) {
+        float f;
+        double d;
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        System.out.println("Float \t: "+ f);
+        System.out.println("Double \t: "+ d);            
+    }
+}
